5d4a8dc8142139869b3dc352024f15657e1ca61f
saraelizalde <j7esg480@students.codeinstitute.net>
2025-08-05 15:37:28 +0100

    Initial setup: installed Django, created apps, tested views

---
+++ dons_table/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for dons_table project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from menu.views import my_menu
+from reservations.views import my_reservation
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('menu/', my_menu, name='menu'),
+    path('reservation/', my_reservation, name='reservation'),
+]
