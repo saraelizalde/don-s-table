c93ffe3829c67038d9d8661ef241cdbab09d22a3
saraelizalde <j7esg480@students.codeinstitute.net>
2025-08-05 17:37:37 +0100

    Prepare project for initial Heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dons_table.wsgi
--- dons_table/settings.py
+++ dons_table/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jyd0b2vob_h3*sq9fs-ynb=z(v^rc(ezw8c=^^q8y7bvt_$8!m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
