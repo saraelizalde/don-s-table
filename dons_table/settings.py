1b646bf5d4d4d56fd9a659b62196e9949da197c3
saraelizalde <j7esg480@students.codeinstitute.net>
2025-08-11 17:45:09 +0100

    Configure SECRET_KEY in env.py and settings.py

--- dons_table/settings.py
+++ dons_table/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jyd0b2vob_h3*sq9fs-ynb=z(v^rc(ezw8c=^^q8y7bvt_$8!m'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
