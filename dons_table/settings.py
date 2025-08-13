a7a45b128a08cf087cdcdaa29111d457633c87df
saraelizalde <j7esg480@students.codeinstitute.net>
2025-08-13 19:37:58 +0100

    Create Reservation model

--- dons_table/settings.py
+++ dons_table/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
@@ -101,6 +101,11 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+from datetime import datetime, timedelta
+
+STATUS = ((0, "Pending"), (1, "Confirmed"))
 
 # Create your models here.
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    end_time = models.TimeField(editable=False)
+    guests = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(14)]
+    )
+    special_requests = models.TextField(blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def save(self, *args, **kwargs):
+        if self.time:
+            self.end_time = (datetime.combine(self.date, self.time) + timedelta(hours=1)).time()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Reservation for {self.user.username} on {self.date} at {self.time}"
+    
+    class Meta:
+        ordering = ['date', 'time']
