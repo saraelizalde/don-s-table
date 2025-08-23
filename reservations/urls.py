48accf93e82650a00c3d88d844e6c46dd5934fe5
saraelizalde <j7esg480@students.codeinstitute.net>
2025-08-23 18:01:30 +0100

    Add edit and cancel functionality to user reservations with styled buttons

--- reservations/urls.py
+++ reservations/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('dashboard/', views.reservation_dashboard, name='reservation_dashboard'),
     path('form/', views.make_reservation, name='make_reservation'),
+    path("<int:reservation_id>/edit/", views.edit_reservation, name="edit_reservation"),
+    path("<int:reservation_id>/cancel/", views.cancel_reservation, name="cancel_reservation"),
 ]
