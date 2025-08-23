a2968815ac0d5285afd71c2fc7f217e75afa777c
saraelizalde <j7esg480@students.codeinstitute.net>
2025-08-23 20:07:58 +0100

    Collect static files and prepare for production

--- dons_table/settings.py
+++ dons_table/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['dons-table-784bf0e01547.herokuapp.com', '.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
