51f21877bd738499afc558c2503b93864d37daf6
saraelizalde <j7esg480@students.codeinstitute.net>
2025-08-29 16:33:58 +0100

    Add test for invalid negative price in Menu app

--- menu/forms.py
+++ menu/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import MenuItem
+from django.core.validators import MinValueValidator
 
 class MenuItemForm(forms.ModelForm):
     """
@@ -12,13 +13,21 @@ class MenuItemForm(forms.ModelForm):
         - category: One of the predefined category choices
         - available: Boolean to mark if item is available
     """
+
+    # Add a minimum value validator to ensure price is positive
+    price = forms.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)],
+        widget=forms.NumberInput(attrs={'class': 'form-control'})
+    )
+
     class Meta:
         model = MenuItem
         fields = ['name', 'description', 'price', 'category', 'available']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-            'price': forms.NumberInput(attrs={'class': 'form-control'}),
             'category': forms.Select(attrs={'class': 'form-control'}),
             'available': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
--- menu/test_forms.py
+++ menu/test_forms.py
@@ -39,4 +39,16 @@ class TestMenuItemForm(TestCase):
         form = MenuItemForm(data=form_data)
         self.assertFalse(form.is_valid(), msg='Form is valid without a price')
 
+    def test_form_is_invalid_with_negative_price(self):
+        """Form should be invalid if the price is negative."""
+        form_data = {
+            'name': 'Margherita Pizza',
+            'description': 'Classic pizza with tomato and mozzarella',
+            'price': '-5.00',
+            'category': 'main',
+            'available': True
+        }
+        form = MenuItemForm(data=form_data)
+        self.assertFalse(form.is_valid(), msg='Form is valid with negative price')
+
   
